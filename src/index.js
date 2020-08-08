45ac1a4a9cdd62e1e47f544daa7239f94a662f64
lawee94 <festac94@gmail.com>
2020-08-08 01:50:46 +0000

    Some Ammendment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "./fonts/Roboto-Bold.ttf";
+import "./fonts/SegoeUI.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
